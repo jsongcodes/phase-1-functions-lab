7d0bee7fa0608663c7f281d04da5bc652e88b321
Jasmin <ssongnari@gmail.com>
2023-07-27 13:26:55 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const headquarters = 42;
+
+const distanceFromHqInBlocks = (location) => {
+  return location >= 42 ? location - headquarters : headquarters - location;
+};
+
+const distanceFromHqInFeet = (location) => {
+  return distanceFromHqInBlocks(location) * 264;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+  return destination >= start
+    ? (destination - start) * 264
+    : (start - destination) * 264;
+};
+
+const calculatesFarePrice = (start, destination) => {
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) <= 1999 && distanceTravelledInFeet(start, destination) >= 401){
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    } else if (distanceTravelledInFeet(start, destination) <= 2499 && distanceTravelledInFeet(start, destination) >= 2001){
+        return 25
+    } else{
+        return 'cannot travel that far'
+    }
+}
